6b5bf2865a8905d453d40f7a5c75c5f056255fbc
iancrosby050 <iancrosby050@gmail.com>
2022-12-21 08:47:03 +0000

    refactored codebase

--- modules/iam-oidc-identity-provider/resource-group.tf
+++ modules/iam-oidc-identity-provider/resource-group.tf
@@ -11,7 +11,7 @@ locals {
 
 
 module "resource_group" {
-  source  = "tedilabs/misc/aws//modules/resource-group"
+  source  = "iancrosby050/microservices-with-aws/modules/resource-group"
   version = "~> 0.10.0"
 
   count = (var.resource_group_enabled && var.module_tags_enabled) ? 1 : 0
